950bbcc601630eaa0eed5759880307a16518f3fa
lukasbrisman <l@brisman.se>
2024-06-23 19:45:45 +0200

    Beskrivande meddelande om dina ändringar

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,26 +2,21 @@ import { defineConfig } from 'astro/config'
 import storyblok from '@storyblok/astro'
 import { loadEnv } from 'vite'
 import tailwind from "@astrojs/tailwind"
-import basicSsl from '@vitejs/plugin-basic-ssl'
+
 const env = loadEnv("", process.cwd(), 'STORYBLOK')
- 
+
 export default defineConfig({
   integrations: [
-    storyblok({
+    storyblok({  
       accessToken: env.STORYBLOK_TOKEN,
-    components: {
-    page: 'storyblok/Page',
-    feature: 'storyblok/Feature',
-    grid: 'storyblok/Grid',
-    teaser: 'storyblok/Teaser',
-  },
+      components: {
+        page: 'storyblok/Page',
+        feature: 'storyblok/Feature',
+        grid: 'storyblok/Grid',
+        teaser: 'storyblok/Teaser',
+      },
     }),
     tailwind()
   ],
-  vite: {
-    plugins: [basicSsl()],
-    server: {
-      https: true,
-    },
-  },
 })
+
